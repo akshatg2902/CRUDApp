5abe0c5df8373fbf421fc94080a0c0b15ef49c12
Akshat Gupta <55349058+akshatg2902@users.noreply.github.com>
2021-10-06 15:47:14 +0530

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,5 @@
 var Userdb = require('../model/model');
 
-// create and save new user
 exports.create = (req,res)=>{
     // validate request
     if(!req.body){
@@ -8,15 +7,13 @@ exports.create = (req,res)=>{
         return;
     }
 
-    // new user
     const user = new Userdb({
         name : req.body.name,
         email : req.body.email,
         gender: req.body.gender,
         status : req.body.status
     })
-
-    // save user in the database
+    
     user
         .save(user)
         .then(data => {
@@ -31,7 +28,6 @@ exports.create = (req,res)=>{
 
 }
 
-// retrieve and return all users/ retrive and return a single user
 exports.find = (req, res)=>{
 
     if(req.query.id){
@@ -62,7 +58,6 @@ exports.find = (req, res)=>{
     
 }
 
-// Update a new idetified user by user id
 exports.update = (req, res)=>{
     if(!req.body){
         return res
@@ -84,7 +79,6 @@ exports.update = (req, res)=>{
         })
 }
 
-// Delete a user with specified user id in the request
 exports.delete = (req, res)=>{
     const id = req.params.id;
 
