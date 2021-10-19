6fb71575356a18ad19214654c1215ca824559cd6
Akshat Gupta <55349058+akshatg2902@users.noreply.github.com>
2021-10-19 13:54:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const app = express();
 dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
 
-// log requests
 app.use(morgan('tiny'));
 
 // mongodb connection
@@ -24,11 +23,9 @@ app.use(bodyparser.urlencoded({ extended : true}))
 app.set("view engine", "ejs")
 //app.set("views", path.resolve(__dirname, "views/ejs"))
 
-// load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-// load routers
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
